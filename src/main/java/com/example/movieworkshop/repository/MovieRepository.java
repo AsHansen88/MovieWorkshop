3bc2ce3df7afdf1fb66203c9488d36655885b0ed
AsHansen88 <Ande970s@stud.kea.dk>
2022-09-09 17:33:53 +0200

    Næsten færdig

---
+++ src/main/java/com/example/movieworkshop/repository/MovieRepository.java
@@ -0,0 +1,59 @@
+package com.example.movieworkshop.repository;
+
+import com.example.movieworkshop.model.Movie;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MovieRepository {
+  private List<Movie> movies = new ArrayList<Movie>();
+
+  public void generateMoviesList() {
+
+    try {
+      Scanner scan;
+      scan = new Scanner(new File("D:\\Datamatiker\\Intellij\\demo\\ERdetfrededag\\MovieWorkshop\\src\\main\\resources\\Movielist"));
+      scan.useDelimiter(";");
+
+
+       scan.nextLine();
+
+      while (scan.hasNextLine()) {
+        String line = scan.nextLine();
+        Scanner token = new Scanner(line).useDelimiter(";").useLocale(Locale.ENGLISH);
+
+        while (token.hasNext()) {
+
+          int year = Integer.parseInt(token.next());
+          int length = Integer.parseInt(token.next());
+          String title = token.next();
+          String subject = token.next();
+          int popularity = Integer.parseInt(token.next());
+          Boolean hasAwards = token.next().equals("Yes");
+
+          movies.add(new Movie(year, length, title, subject, popularity, hasAwards));
+        }
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public Movie getOne(int index) {
+    return movies.get(index);
+  }
+
+  public int getSize() {
+    return movies.size();
+  }
+
+  public List<Movie> getMovies() {
+    return movies;
+  }
+
+
+}
